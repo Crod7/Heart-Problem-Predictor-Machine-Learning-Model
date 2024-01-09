2a7df85cd9ca841a3ab37751f2a9654daeef341e
Carlos Rodriguez <crodriguezyt10@gmail.com>
2024-01-08 23:15:22 -0800

    Added requirements.txt to deploy backend on render.

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+# Imports
+from fastapi import FastAPI, Body, HTTPException
+from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 
+# from routes import customers
+
+
+# Init the server
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+# Root Endpoint
+@app.get("/", tags=["Main"])
+async def get_customers():
+    response = {"message": "hi"}
+    return response
+
+
+# Manages all Customer Endpoints, see customers.py in route folder
+# app.include_router(customers.router, prefix="/api/customers")
